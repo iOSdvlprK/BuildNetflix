d6615b194d71a53afcca4ecbdaba23694bc62e4f
iOSdvlprK <110379993+iOSdvlprK@users.noreply.github.com>
2023-10-25 21:06:10 +0900

    Initial Commit

---
+++ BuildNetflix/BuildNetflixApp.swift
@@ -0,0 +1,17 @@
+//
+//  BuildNetflixApp.swift
+//  BuildNetflix
+//
+//  Created by joe on 2023/10/25.
+//
+
+import SwiftUI
+
+@main
+struct BuildNetflixApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
