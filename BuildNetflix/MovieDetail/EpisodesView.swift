40b535f50d816afffa00622dbd7fa147b60dee01
iOSdvlprK <110379993+iOSdvlprK@users.noreply.github.com>
2023-10-31 22:41:27 +0900

    MovieDetail's state variables and EpisodesView's binding variables; constant in previews

---
+++ BuildNetflix/MovieDetail/EpisodesView.swift
@@ -0,0 +1,57 @@
+//
+//  EpisodesView.swift
+//  BuildNetflix
+//
+//  Created by joe on 2023/10/31.
+//
+
+import SwiftUI
+
+struct EpisodesView: View {
+    var episodes: [Episode]
+    
+    @Binding var showSeasonPicker: Bool
+    @Binding var selectedSeason: Int
+    
+    func getEpisodes(forSeason season: Int) -> [Episode] {
+        return episodes.filter({ $0.season == season })
+    }
+    
+    var body: some View {
+        VStack {
+            // season picker
+            HStack {
+                Button(action: {
+                    showSeasonPicker = true
+                }, label: {
+                    Group {
+                        Text("Season 1")
+                        Image(systemName: "chevron.down")
+                    }
+                    .font(.system(size: 16))
+                })
+                
+                Spacer()
+            }
+            
+            // episode list
+            ForEach(getEpisodes(forSeason: selectedSeason)) { episode in
+                Text(episode.name)
+            }
+            
+            Spacer()
+        }
+        .foregroundColor(.white)
+    }
+}
+
+struct EpisodesView_Previews: PreviewProvider {
+    static var previews: some View {
+        ZStack {
+            Color.black
+                .edgesIgnoringSafeArea(.all)
+            
+            EpisodesView(episodes: allExampleEpisodes, showSeasonPicker: .constant(false), selectedSeason: .constant(1))
+        }
+    }
+}
