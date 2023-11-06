b14a2b50bb510910371f4e3f139897fd0041d9ff
iOSdvlprK <110379993+iOSdvlprK@users.noreply.github.com>
2023-11-06 22:16:09 +0900

    lay out UI of custom search bar

---
+++ BuildNetflix/Helpers/Colors.swift
@@ -0,0 +1,13 @@
+//
+//  Colors.swift
+//  BuildNetflix
+//
+//  Created by joe on 2023/11/06.
+//
+
+import SwiftUI
+
+extension Color {
+    static let graySearchBackground = Color("gray_search_background")
+    static let graySearchText = Color("gray_search_text")
+}
