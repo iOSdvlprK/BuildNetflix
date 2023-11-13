839aa7cfa993b854200caf7ade76e63f3ecf7f5f
iOSdvlprK <110379993+iOSdvlprK@users.noreply.github.com>
2023-11-13 22:13:46 +0900

    MoviePreviewRow

---
+++ BuildNetflix/Preview/MoviePreviewRow.swift
@@ -0,0 +1,47 @@
+//
+//  MoviePreviewRow.swift
+//  BuildNetflix
+//
+//  Created by joe on 2023/11/13.
+//
+
+import SwiftUI
+
+struct MoviePreviewRow: View {
+    var movies: [Movie]
+    
+    var body: some View {
+        VStack(alignment: .leading) {
+            Text("Previews")
+                .font(.title3)
+                .bold()
+                .foregroundColor(.white)
+                .padding(.horizontal, 6)
+            
+            ScrollView(.horizontal, showsIndicators: false) {
+                LazyHStack {
+                    ForEach(0..<movies.count, id: \.self) { movieIndex in
+                        let movie = movies[movieIndex]
+                        
+                        MoviePreviewCell(movie: movie)
+                            .frame(width: 120, height: 120)
+                            .padding(.trailing, 14)
+                            .padding(.leading, 6)
+                    }
+                }
+            }
+        }
+        .frame(height: 185)
+    }
+}
+
+struct MoviePreviewRow_Previews: PreviewProvider {
+    static var previews: some View {
+        ZStack {
+            Color.black
+                .edgesIgnoringSafeArea(.all)
+            
+            MoviePreviewRow(movies: exampleMovies)
+        }
+    }
+}
