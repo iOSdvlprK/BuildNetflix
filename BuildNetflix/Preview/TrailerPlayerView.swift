0f17dd6dd0f4a1c528f3b9dbcd6fa78bfd97b81e
iOSdvlprK <110379993+iOSdvlprK@users.noreply.github.com>
2023-11-15 22:44:41 +0900

    TrailerPlayerView, video player wrapper

---
+++ BuildNetflix/Preview/TrailerPlayerView.swift
@@ -0,0 +1,29 @@
+//
+//  TrailerPlayerView.swift
+//  BuildNetflix
+//
+//  Created by joe on 2023/11/15.
+//
+
+import SwiftUI
+import VideoPlayer
+
+struct TrailerPlayerView: View {
+    var videoURL: URL?
+    
+    @Binding var playVideo: Bool
+    
+    var body: some View {
+        if videoURL != nil {
+            VideoPlayer(url: videoURL!, play: $playVideo)
+        } else {
+            Text("Could not load video")
+        }
+    }
+}
+
+struct TrailerPlayerView_Previews: PreviewProvider {
+    static var previews: some View {
+        TrailerPlayerView(videoURL: nil, playVideo: .constant(true))
+    }
+}
